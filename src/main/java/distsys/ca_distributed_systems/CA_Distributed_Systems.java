9836f9c88023c9fad5c01f1cd8260314697c5453
Kirill Tkachenko <kirilltkacenko37@gmail.com>
2026-07-10 17:20:00 +0100

    Start of project, protos and verification

---
+++ src/main/java/distsys/ca_distributed_systems/CA_Distributed_Systems.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package distsys.ca_distributed_systems;
+
+/**
+ *
+ * @author Kirill
+ */
+public class CA_Distributed_Systems {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
