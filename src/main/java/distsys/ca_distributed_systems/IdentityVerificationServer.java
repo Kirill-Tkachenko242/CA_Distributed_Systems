0edd9156492d801b292944e2cda3a9e432aeac26
Kirill Tkachenko <kirilltkacenko37@gmail.com>
2026-07-29 10:27:08 +0100

    Identity verification (server and client)

---
+++ src/main/java/distsys/ca_distributed_systems/IdentityVerificationServer.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package distsys.ca_distributed_systems;
+
+/**
+ *
+ * @author Kirill
+ */
+public class IdentityVerificationServer {
+    
+}
