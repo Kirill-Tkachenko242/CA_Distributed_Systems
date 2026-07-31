8e09329f91bd158cc66141a2b51fc8a207ce484a
Kirill Tkachenko <kirilltkacenko37@gmail.com>
2026-07-31 09:58:43 +0100

    discovery

--- src/main/java/distsys/ca_distributed_systems/ServiceDiscovery.java
+++ src/main/java/distsys/ca_distributed_systems/ServiceDiscovery.java
@@ -4,10 +4,53 @@
  */
 package distsys.ca_distributed_systems;
 
+import java.io.IOException;
+import java.net.InetAddress;
+import java.util.concurrent.ConcurrentHashMap;
+import javax.jmdns.JmDNS;
+import javax.jmdns.ServiceEvent;
+import javax.jmdns.ServiceInfo;
+import javax.jmdns.ServiceListener;
+
 /**
  *
  * @author Kirill
  */
 public class ServiceDiscovery {
-
+    
+    private JmDNS jmdns;
+    private ConcurrentHashMap<String, ServiceInfo> discoveredServices = new ConcurrentHashMap<>();
+    
+    public ServiceDiscovery() throws IOException {
+        this.jmdns = JmDNS.create(InetAddress.getLocalHost());
+        
+        jmdns.addServiceListener(Serviceregistrar.SERVICE_TYPE, new ServiceListener() {
+            @Override
+            public void serviceAdded(ServiceEvent event) {
+                jmdns.requestServiceInfo(event.getType(), event.getName(), 3000);
+            }
+            
+            @Override
+            public void serviceResolved(ServiceEvent event) {
+                ServiceInfo info = event.getInfo();
+                discoveredServices.put(info.getName(), info);
+                System.out.println("[jmDNS] Discovered service \"" + info.getName()
+                        + "\" at " + info.getHostAddresses()[0] + ":" + info.getPort());
+            }
+            
+            @Override
+            public void serviceRemoved(ServiceEvent event) {
+                discoveredServices.remove(event.getName());
+                System.out.println("[jmDNS] Service \"" + event.getName() + "\" went offline");
+            }
+        });
+    }
+    
+    public ServiceInfo getService(String serviceName) {
+        return discoveredServices.get(serviceName);
+    }
+    
+    public void close() throws IOException {
+        jmdns.close();
+    }
 }
